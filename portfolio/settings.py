45f25b67fdb6a8b9e80701e5b618f4f5643d1d4a
Gabrielle Harrison <gabrielle@codesters.com>
2017-06-09 14:07:19 -0400

    changing static files path

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,12 +138,12 @@ if DEBUG:
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/staticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
     STATIC_URL = '/static/'
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'portfolio/static'),
+        os.path.join(BASE_DIR, 'portfolio/static/staticfiles'),
     )
 
     WEBPACK_LOADER = {
