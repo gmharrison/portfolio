fca0f70cb2ce759684201c3c2f9623dddabaaeb8
Gabrielle Harrison <gabrielle@codesters.com>
2017-08-08 10:23:17 -0400

    added more entries to art roulette

--- portfolio/static/js/roulette_data.js
+++ portfolio/static/js/roulette_data.js
@@ -78,6 +78,57 @@ export const data = [{
     title: "St. Sebastian Thrown into the Cloaca Maxima",
     artist: "Lodovico Carracci",
     date: "1612",
-
+    text: "Against the dark of night, brutish soldiers lift and tug the dead saint's body. Ludovico contrasted the tensile strength of their straining bodies " +
+    "with the slackness of the saint's limbs, head, and facial muscles as he falls into the sewer's depths. The night atmosphere " +
+    "is dark and thick: figures seem to emerge from the blackness. Light glints dully off helmets and armor, but the soldiers' " +
+    "faces are unreadable. A bright light suffuses the body of Saint Sebastian, making him the focal point of the composition. " +
+    "In 1612 Cardinal Maffeo Barberini commissioned this painting from Ludovico for his family's chapel in the Church of San Andrea " +
+    "della Valle in Rome. The chapel commemorated the site where Saint Sebastian's body was recovered from the sewer, called the Cloaca " +
+    "Maxima. Barberini decided to keep the painting in his private collection, believing that an image of the recovery of Sebastian's body" +
+    " by Christians was more appropriate for the church.",
+    image: `${STATIC_URL}/img/roulette/sebastian.jpg`,
+    source: "the J. Paul Getty Museum",
+    link: "http://www.getty.edu/art/collection/objects/617/lodovico-carracci-st-sebastian-thrown-into-the-cloaca-maxima-italian-1612/"
+    }, {
+    id: 6,
+    title: "The Penitence of Saint Jerome",
+    artist: "Joachim Patinir",
+    date: "ca. 1512–15",
+    text: "Acknowledging Patinir’s leading role in a new genre, Albrecht Dürer referred to the artist in 1521 as the 'good landscape painter.'" +
+    " This intact altarpiece was probably a German commission, since its exterior wings show Sebald, patron saint of Nuremberg, and " +
+    "Saint Anne with the Virgin and Child. Following Netherlandish tradition, large-scale sacred figures dominate the foreground of " +
+    "the interior: Saint John baptizing Christ, Saint Jerome, and Saint Anthony the Hermit with the monsters that assailed him. " +
+    "The picture’s true subject, however, is the magnificent panoramic landscape, which the viewer is encouraged to travel through visually " +
+    "in the manner of a pilgrimage.",
+    image: `${STATIC_URL}/img/roulette/jerome.jpg`,
+    source: "Metropolitan Museum of Art",
+    link: "http://www.metmuseum.org/art/collection/search/437261"
+    }, {
+    id: 7,
+    title: "Vessel, Mythological Scene",
+    date: "7th–8th century",
+    text: "The scene here is not fully understood, but represents one variation of a theme depicted on several other codex-style " +
+    "vessels in public and private collections. Sometimes the rain god is shown with a human face, sometimes the death god has " +
+    "the distended belly of a corpse, and sometimes the anthropomorphic jaguar is shown as an infant on its back, such as in the " +
+    "Metropolitan Museum’s 'Baby Jaguar' vessel (1978.412.206). In other scenes the protagonists are accompanied by creatures of the night, " +
+    "such as fireflies, toads, dogs, or other deities. The artists painting these vases might be showing different scenes in a larger narrative, " +
+    "or the same scene depicted in very different ways.",
+    image: `${STATIC_URL}/img/roulette/vessel.jpg`,
+    source: "Metropolitan Museum of Art",
+    link: "http://www.metmuseum.org/art/collection/search/314217"
+    }, {
+    id: 8,
+    title: "Delightful Land",
+    artist: "Paul Gauguin",
+    date: "1893–94",
+    text: "Gauguin spent much of his time in Paris in 1893–94 working on the text and illustrations for Noa Noa (Fragrance). " +
+    "The project began as a narrative to guide an uncomprehending public through Gauguin's personal and artistic perceptions " +
+    "of Tahiti, but ultimately became more complex and mysterious than anything he had produced in the South Seas. The " +
+    "remarkable series of ten woodblock prints that Gauguin designed as illustrations bear little relationship to his " +
+    "romanticized autobiographical text. Nor do they seem to follow any particular sequence. However, the imagery and " +
+    "themes of the prints—love and fear, creation and death, day and night—relate closely to those of Gauguin's Tahitian paintings.",
+    image: `${STATIC_URL}/img/roulette/gauguin.jpg`,
+    source: "Metropolitan Museum of Art",
+    link: "http://www.metmuseum.org/art/collection/search/337852"
 }
 ]
